ad420c23075d77b697607bd6553b9d0475a64126
GeniusFunny <865753437@qq.com>
2018-05-09 22:58:53 +0800

    add mysql

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const mysql = require('mysql')
 
 const app = new express()
 const port = process.env.PORT || 5000
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '19980812',
+  database: 'bookstore',
+  port: '3306'
+})
 
+connection.connect()
+
+connection.query('SELECT * FROM user;', function(err, res) {
+  if (err) {
+    console.log(err)
+  }
+  console.log('打印数据库')
+  console.log(res[0])
+})
+
+connection.end()
 app.get('/api/hello', (req, res) => {
   res.send({
     express: 'Hello, from Express'
